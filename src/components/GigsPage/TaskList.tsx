2589d1e318bdcb58730dc34e7996255868217253
v2vnetwork <v2vnetwork@protonmail.com>
2025-04-05 18:05:47 -0400

    Images fix

--- src/components/GigsPage/TaskList.tsx
+++ src/components/GigsPage/TaskList.tsx
@@ -5,7 +5,7 @@ import { TaskCard } from "./TaskCard";
 const tasks = [
   { 
     image:
-      "/assets/girl.png",
+      "/assets/man-1.jpg",
     author: "Robert.T",
     title: "Looking for a dog sitter in Thornhill area",
     rate: "$16-22/hr",
@@ -14,7 +14,7 @@ const tasks = [
   },
   {
     image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/e64b4003e240951935fe661bbc034c223290fc3f",
+      "/assets/woman-1.jpg",
     author: "Louis.S",
     title: "Assemble and install furniture with precision",
     rate: "$19-20/hr",
@@ -23,7 +23,7 @@ const tasks = [
   },
   {
     image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/9d49b9b491990b4ab342e142f85e4c8be9d07f9f",
+      "/assets/woman-2.jpg",
     author: "Sarah.S",
     title: "Care for dogs, including feeding and walking",
     rate: "$22-24/hr",
@@ -32,7 +32,7 @@ const tasks = [
   },
   {
     image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/43df7813b3ec2276fd46b2a9ae8400282cec53ae",
+      "/assets/image-275.png",
     author: "Leila.R",
     title: "Shop for groceries and deliver them on time",
     rate: "$15-18/hr",
@@ -41,7 +41,7 @@ const tasks = [
   },
   {
     image:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/a5bfaa69fb2b9e953351e1577c051787f672079b",
+      "/assets/woman-3.jpg",
     author: "Bethany.S",
     title: "Maintain and enhance outdoor plants and gardens",
     rate: "$16-22/hr",
